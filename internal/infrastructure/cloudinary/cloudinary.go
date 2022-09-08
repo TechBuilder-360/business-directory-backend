4ad63a25feda8e4f900a6a1a2d12fbae0577d5fa
folayan shola <folayanshola@gmail.com>
2022-09-08 09:28:34 +0100

    changes

--- internal/infrastructure/cloudinary/cloudinary.go
+++ internal/infrastructure/cloudinary/cloudinary.go
@@ -1,4 +1,4 @@
-package utility
+package cloudinary
 
 import (
 	"context"
--- internal/services/user.go
+++ internal/services/user.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"github.com/TechBuilder-360/business-directory-backend/internal/common/constant"
 	"github.com/TechBuilder-360/business-directory-backend/internal/common/types"
-	"github.com/TechBuilder-360/business-directory-backend/internal/infrastructure/utility"
+	"github.com/TechBuilder-360/business-directory-backend/internal/infrastructure/cloudinary"
 	"github.com/TechBuilder-360/business-directory-backend/internal/model"
 	"github.com/TechBuilder-360/business-directory-backend/internal/repository"
 	log "github.com/sirupsen/logrus"
@@ -26,7 +26,7 @@ func NewUserService() UserService {
 
 func (r *DefaultUserService) UpgradeTierOne(body *types.UpgradeUserTierRequest, logger *log.Entry) error {
 	user := &model.User{}
-	url, err := utility.ImageUpload(body.IdentityImage)
+	url, err := cloudinary.ImageUpload(body.IdentityImage)
 	if err != nil {
 		logger.Error(err)
 		return errors.New(constant.InternalServerError)
